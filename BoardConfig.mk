8cabd088be754d91adc7e297b9479d5c74c18701
Jan Huijsmans <huysmans@koffie.nu>
2016-07-04 14:52:39 +0200

    Correct dep and partitions
    Change-Id: I6b0506d8ecfa309286330a90a4445eb8a7a62cf7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include device/motorola/msm8916-common/BoardConfigCommon.mk
+include device/motorola/msm8952-common/BoardConfigCommon.mk
 
 -include vendor/motorola/athene/BoardConfigVendor.mk
 
@@ -31,11 +31,11 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_athene
 TARGET_KERNEL_CONFIG := athene_defconfig
 
 # Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216     # 16384 * 1024 mmcblk0p31
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16879616 # 16484 * 1024 mmcblk0p32
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2432696320 # 2375680 * 1024 mmcblk0p41
-BOARD_PERSISTIMAGE_PARTITION_SIZE := 8388608   # 8192 * 1024 mmcblk0p29
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 4865261568 # 4751232 * 1024 mmcblk0p42
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864        # 16384 * 4096 mmcblk0p28
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864    # 16484 * 4096 mmcblk0p29
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 10737418240   # 2621440 * 4096 mmcblk0p47
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 134217728    # 32768 * 4096 mmcblk0p30
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 46497529856 # 11351936 * 4096 mmcblk0p48
 
 # Power
 TARGET_POWERHAL_VARIANT := qcom
