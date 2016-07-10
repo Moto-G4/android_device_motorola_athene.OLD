48201626ddca8f8e1570c516ba49970b4b964b78
Jan Huijsmans <huysmans@koffie.nu>
2016-07-10 12:10:41 +0200

    SoC is 64 bit

--- cm.mk
+++ cm.mk
@@ -20,6 +20,7 @@ $(call inherit-product, device/motorola/athene/full_athene.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common CM stuff.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/telephony.mk)
 
